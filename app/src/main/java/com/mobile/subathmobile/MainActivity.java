e3261f7d568d440c6f5b2d95fbad2bc37e69daf3
RishniMeemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2017-12-15 19:38:13 +0530

    Initial commit

---
+++ app/src/main/java/com/mobile/subathmobile/MainActivity.java
@@ -0,0 +1,30 @@
+package com.mobile.subathmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebview=(WebView)findViewById(R.id.webView);
+        WebSettings webSettings = mywebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebview.loadUrl("http://subathamaradasa.ml/");
+        mywebview.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mywebview.canGoBack()) {
+            mywebview.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
